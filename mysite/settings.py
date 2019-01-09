d7eb0ca9887f561f3599ad4898a1eb94806234c7
vinay <vkgv@django.com>
2019-01-09 19:37:27 +0530

    debugt

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4hpc4i2xo5xvrfz*l#e#lh%yuqa9)0^1a$ayzs$zj!8e3qq%c@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', '.pythonanywhere.com','localhost']
 
